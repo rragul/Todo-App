e6ee4b2809ef1032c7f2bafbe6eb8a16975fc0db
rragul <r.ragul0598@gmail.com>
2022-11-13 18:12:37 +0530

    edit code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const initialState = JSON.parse(localStorage.getItem("todos")) || [];
   const [input, setInput] = useState("");
   const [todos, setTodos] = useState(initialState);
-  const [editTodo, setEditTodo] = useState([]);
+  const [editTodo, setEditTodo] = useState(null);
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
